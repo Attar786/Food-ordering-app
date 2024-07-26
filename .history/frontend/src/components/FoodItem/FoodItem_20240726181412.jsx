405fac7e0c634a39214bc8d784510ce3f0bc2554
Atta ur Rehman <attaurr786@outlook.com>
2024-07-26 19:03:44 +0500

    counter button

---
+++ .history/frontend/src/components/FoodItem/FoodItem_20240726181412.jsx
@@ -0,0 +1,36 @@
+import './FoodItem.css';
+import { assets } from '../../assets/assets'; // Ensure this path is correct and that assets contains rating_starts
+import {useState} from 'react'
+const FoodItem = ({ id, name, price, description, image }) => {
+
+
+  const [itemCount, SetItemCount] = useState(0)
+  return (
+    <div className='food-item'>
+      <div className='food-item-img-container'>
+        <img className='food-item-image' src={image} alt={name} />
+
+        {
+
+!itemCount 
+? <img className='add' onClick={()=>SetItemCount(prev=>prev+1)} src='{assets.add_icon_white}' alt = ""/>
+:<div>
+
+</div>
+
+        }
+      </div>
+      <div className='food-item-info'>
+        <div className='food-item-name-rating'>
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="Rating" />
+        </div>
+        <p className='food-item-description'>
+          {description}
+        </p>
+        <p className='food-item-price'>RS{price}</p>      </div>
+    </div>
+  );
+};
+
+export default FoodItem;
--- .history/frontend/src/components/FoodItem/FoodItem_20240726181927.jsx
+++ .history/frontend/src/components/FoodItem/FoodItem_20240726181927.jsx
@@ -1,22 +1,21 @@
 import './FoodItem.css';
 import { assets } from '../../assets/assets';
-import { useState } from 'react';
-
+import {useState} from 'react'
 const FoodItem = ({ id, name, price, description, image }) => {
-  const [itemCount, setItemCount] = useState(0);
 
+
+  const [itemCount, SetItemCount] = useState(0)
   return (
     <div className='food-item'>
       <div className='food-item-img-container'>
         <img className='food-item-image' src={image} alt={name} />
         {
-          !itemCount 
-            ? <img className='add' onClick={() => setItemCount(prev => prev + 1)} src={assets.add_icon_white} alt="" />
-            : <div className='food-item-counter'>
-                <img onClick={() => setItemCount(prev => prev > 0 ? prev - 1 : 0)} src={assets.remove_icon_red} alt="" />
-                <p>{itemCount}</p>
-                <img onClick={() => setItemCount(prev => prev + 1)} src={assets.add_icon_green} alt="" />
-              </div>
+!itemCount 
+?<img className='add' onClick={()=>SetItemCount(prev=>prev+1)} src='{assets.add_icon_white}' alt = ""/>
+:<div>
+
+</div>
+
         }
       </div>
       <div className='food-item-info'>
@@ -27,8 +26,7 @@ const FoodItem = ({ id, name, price, description, image }) => {
         <p className='food-item-description'>
           {description}
         </p>
-        <p className='food-item-price'>RS{price}</p>
-      </div>
+        <p className='food-item-price'>RS{price}</p>      </div>
     </div>
   );
 };
